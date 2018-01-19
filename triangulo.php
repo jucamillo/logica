14313bb2a8f647cdac574431cabf4ab6e09735fe
php 04 <php.04@lampadinha.cec.ime.usp.br>
2018-01-19 21:55:14 -0200

    soluçoes triangulo

--- triangulo.php
+++ triangulo.php
@@ -3,3 +3,18 @@ for ($i=0; $i < 5; $i++){
   $number = ($i + 1);
   echo str_repeat ("*", $number). PHP_EOL;
 }
+echo "solucao1\n";
+for ($i=1; $i <= 5; $i++){
+  $k = $i;
+  for($j=0; $j < $k; $j++){
+    echo "*";
+  }
+  echo "\n";
+}
+echo "solucao2\n";
+
+$asterisco = "*";
+for ($i=0; $i < 5; $i++){
+  echo $asterisco . PHP_EOL;
+  $asterisco .= "*";
+}
